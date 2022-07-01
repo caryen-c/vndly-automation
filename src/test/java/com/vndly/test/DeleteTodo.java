ea0db06a3092d08a23971d6eea2e517e283facbb
Caryen Cheong <caryen.cheong@sita.aero>
2022-07-01 11:30:22 +0800

    added test code to delete todo

--- src/test/java/com/vndly/test/DeleteTodo.java
+++ src/test/java/com/vndly/test/DeleteTodo.java
@@ -1,12 +1,22 @@
 package com.vndly.test;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DeleteTodo {
 	
 	@Test
 	public void deleteTodo() {
 		//your code here
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\src\\test\\resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://qa-challenge.beta.vndly.com/");
+		WebElement deleteBtn = driver.findElement(By.xpath("//button[@data-testid='todo-list-item-delete']"));
+		deleteBtn.click();
+		System.out.println("You have deleted a todo. Yay!");
 	}
-
+	
 }
